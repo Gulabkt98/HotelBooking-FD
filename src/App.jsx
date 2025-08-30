1532999128bf8168e3c849e7c34a0f3e866bf805
Gulaba  Ram <gulabgkg99@gmail.com>
2025-08-30 13:54:39 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Footer from './Componenets/Footer'
 import AllRooms from './pages/AllRooms'
 import RoomDetails from './pages/RoomDetails'
 import MyBookings from './pages/MyBookings'
+import HotelReg from './Componenets/HotelReg'
+import Layout from './pages/hotelOwner/Layout'
 
 const App = () => {
 
@@ -14,12 +16,17 @@ const App = () => {
   return (
     <div>
      { !isOwnerpath &&  <Navbar /> }
+      {false && <HotelReg /> }
      <div className='min-h-[70vh]'>
       <Routes>
         < Route path='/' element={<Home/>} /> 
         < Route path='/rooms' element={<AllRooms/>} />
         < Route path='/rooms/:id' element={<RoomDetails/>} />
-        < Route path='/my-bookings' element={<MyBookings/>} />  
+        < Route path='/my-bookings' element={<MyBookings/>} /> 
+        <Route path='/owner' element={Layout}>
+
+
+        </Route> 
       </Routes>
      </div>
      <Footer />
